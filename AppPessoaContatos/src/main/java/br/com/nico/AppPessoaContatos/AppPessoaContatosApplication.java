45b63803fd7ba9734e4880de8ffce8c063cb7ea7
Nicolaos Tsoukalas <nicotsoukalas@gmail.com>
2024-07-04 12:07:25 -0300

    Commit inicial

---
+++ AppPessoaContatos/src/main/java/br/com/nico/AppPessoaContatos/AppPessoaContatosApplication.java
@@ -0,0 +1,13 @@
+package br.com.nico.AppPessoaContatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPessoaContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPessoaContatosApplication.class, args);
+	}
+
+}
---
+++ AppPessoaContatos/src/test/java/br/com/nico/AppPessoaContatos/AppPessoaContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nico.AppPessoaContatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPessoaContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
